d62fb79a035c1a272bc080d3510c3abb40800684
Nandan Kumar <nandankmrde@gmail.com>
2022-11-26 11:31:29 +0530

    navD

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views')
@@ -22,5 +23,8 @@ db.on('error' , error => console.log(error))
 db.once('open' , error => console.log("Connected to mongoose"))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+
+
 app.listen(process.env.PORT || 3003)
 

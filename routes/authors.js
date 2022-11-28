41975d3bae709f1a51a0e78a4365149e0c7a9ea6
Nandan Kumar <nandankmrde@gmail.com>
2022-11-29 02:53:39 +0530

    authorDn

--- routes/authors.js
+++ routes/authors.js
@@ -1,35 +1,69 @@
 const express = require('express')
+const author = require('../models/author')
 const router = express.Router()
 const Author = require('../models/author')
 //All Authors Route
-router.get('/',(req,res) =>{
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    
+    let searchOptions = {}
+    /*
+    GET -----query
+    POST-----body
+     */
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name ,'i')
+    }
+    
+    try{
+      const authors = await Author.find(searchOptions) 
+      
+      res.render('authors/index' , {authors : authors , searchOptions : req.query})
+    
+    }
+    catch{
+      res.redirect('/')
+    }
 })
 
 //New Author Route
-router.get('/new',(req,res) =>{
-    res.render('authors/new',{author : new Author()})
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
 })
 
 //Create Author route
-router.post('/', (req,res) => {
+router.post('/', async (req, res) => {
     const author = new Author({
-        name : req.body.name
+        name: req.body.name
     });
-    author.save((err, newAuthor) =>{
-       if(err)
-       {
-        res.render('authors/new' , {
-            author : author,
-            errorMessage : 'Error Creating and Author'
-        })
-       } else{
-        //res.redirect(`authors/${newAuthor.id}`)
+
+    try {
+        const newAuthor = await author.save()
+       // res.redirect(`authors/${newAuthor.id}`)
         res.redirect(`authors`)
-          
     }
-    })
-    
+    catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error Creating and Author'
+        })
+
+    }
+
+
+    // author.save((err, newAuthor) =>{
+    //    if(err)
+    //    {
+    //     res.render('authors/new' , {
+    //         author : author,
+    //         errorMessage : 'Error Creating and Author'
+    //     })
+    //    } else{
+    //     //res.redirect(`authors/${newAuthor.id}`)
+    //     res.redirect(`authors`)
+
+    // }
+    // })
+
 
 })
 
